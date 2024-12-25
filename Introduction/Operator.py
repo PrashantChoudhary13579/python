f9566de535384f825f5f500e7e4c25472e44fb43
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2024-12-25 22:51:13 +0530

    Learning about operator in python

---
+++ Introduction/Operator.py
@@ -0,0 +1,43 @@
+#In this file we will learn about the operator in the python and also many more things...
+# Operands, operators
+# Arithmetic operators
+a = 5 
+b = 2
+sum = a+b
+# print (sum)
+# print (a+b)
+# print (a-b)
+# print (a*b)
+# print (a/b)
+# print (a**b)
+# print (a%b)
+
+# Relational Operator
+# print(a>b) # True
+# print(a<b) # False
+# print(a==b)# False
+# print(a!=b)# True
+
+# assignment operator
+num = 10 
+# num = num +10
+print("num :",num)
+num -= 5
+print("num :",num)
+
+# Logical operator
+
+print(not False)
+print(not True)
+
+a = 50 
+b = 30 
+print(not (a>b)) # False
+
+# and + or operator
+val1 = True
+val2 = True
+print("and operator: ", not val1 and val2)
+print("and operator: ", not val1 or val2)
+print("and operator: ", not val1 or not val2)
+print("answer is : ", (a==b) or (a>b))
