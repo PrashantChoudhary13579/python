6ec35c0482a0b4127028d8c01725b8bd656df444
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2024-12-25 20:35:55 +0530

    Initial commit

---
+++ firstFile.py
@@ -0,0 +1,47 @@
+# print("Hello, Hacker....")
+# print("This is open world. Let's hack it.")
+# print("Learning python","is good play with it","or","play with the world by hacking")
+
+#variables
+name = "Prashant Choudhary"
+age = 20
+interest ="hacking"
+bug = 404.404 #decimal
+# applying the assignment operator
+age2 =age +3
+
+#printing
+
+print ("My name is",name)
+print ("My age is",age2)
+print ("My interest is in",interest)
+
+#identifiers - names of the variables 
+# it can't start with digit, also should not start with special character, spaces between them
+# name should be simple, and easy to understand 
+
+print (type(name))
+print (type(age))
+print (type(bug))
+
+# Data Types in Python
+# integer - values , string as below , float-contain decimal, boolean - true/false, none, 
+
+name1 = "Prashant"
+name2 = 'Choudhary'
+name3 = '''Gujjar'''
+print(name1)
+print(name2)
+print(name3)
+# But usually we will use the dubble cot 
+
+old= False
+a = None
+print(False)
+print(a)
+print(type(old))
+print(type(a))
+
+#keywords - are the reserved words, we can't use them as variable name. 
+# Python is case sensitive - capital and small letters are treated as differently
+# SQl is not a case sensitive language.
