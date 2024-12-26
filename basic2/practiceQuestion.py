f9126851f028630b76379ec21048f760342baaab
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2024-12-26 22:27:51 +0530

    practice questions

---
+++ basic2/practiceQuestion.py
@@ -0,0 +1,35 @@
+#1. WAP to input user's first name and print its length.
+# userName = input("Enter your sweet Name: ")
+# print(len(userName))
+
+#2. WAP to find the occurence of '$' in a String.
+str = "fiewfb2#$@^$^%#^$@%#&&"
+print(str.count('$'))
+
+#3. WAP to check if a number entered by the user is odd or even.
+num = int(input("Enter your number "))
+if(num % 2 == 0):
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
+# #4. WAP to find the greatest of three numbers entered by the user.
+# num1 = float(input("Enter the first number"))
+# num2 = float(input("Enter the second number"))
+# num3 = float(input("Enter the third number"))
+
+# if(num1 > num2 and num1 > num3):
+#     print(num1,"is the greatest.")
+# elif(num2 > num1 and num2 >num3):
+#     print(num2,"is the greatest.")
+# else:
+    # print(num3,"is the greatest of all.")
+
+#5. WAP to check if a number is a multiple of 7 or not.
+value = int(input("Enter the number"))
+if(value%7 == 0):
+    print("value is multiple of 7.")
+else:
+    print("value is not a multiple of 7.")
+
+#6. largest of 4 number.
