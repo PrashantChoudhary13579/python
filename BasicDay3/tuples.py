718a7762b08e061fc72a7badb33226132f0814d8
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2024-12-27 17:50:59 +0000

    Learning about tuple in python

---
+++ BasicDay3/tuples.py
@@ -0,0 +1,8 @@
+# Tuple - it is a built-in data type that lets us create immutate sequences of values.
+tup = (8,3,6,2)
+print(tup)
+print(type(tup))
+
+print("Value at index 2 =",tup.index(2)) # will give the value at that position
+print("No. of occurence of 2 =",tup.count(2)) # finding the number of occurences
+
