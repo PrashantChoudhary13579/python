91795b5fabaeccc23d589c78a01e2cf7069f50fb
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2024-12-29 18:28:33 +0000

    Practicing question

---
+++ BasicDay4/practice-Ques.py
@@ -0,0 +1,13 @@
+# 1. Storing the word meanings in a python dictionary
+dictionary = {
+    "table" : "a piece of furniture",
+    "cat " : "a small animal",
+    
+}
+print(dictionary)
+
+# 2. You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.
+
+list = ["python","java","c++","python","javascript","java","python","java","c++","C"]
+print(list)
+print()
