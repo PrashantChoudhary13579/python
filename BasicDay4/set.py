c79aa24a79b2c6656cc9604762001981d0ec8963
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2024-12-29 17:57:14 +0000

    Learning set

---
+++ BasicDay4/set.py
@@ -0,0 +1,16 @@
+# set is the collection of the unordered items- koi order follow nahi hota jo last mein tha vo phele ya bhich mein bhi aa sakta h
+# Each element in the set must be unique and immutable.
+num = {1,2,3,4}
+set2 = {1,2,3,4}
+
+print(num)
+print(type(num))
+
+print(len(num))
+
+# Dublicate values will be ignored
+
+null_set = set()
+print(null_set)
+
+print(type(num))
