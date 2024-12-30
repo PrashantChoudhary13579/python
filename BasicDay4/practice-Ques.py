fb56c5e3d8ffa87b1436dc2c12c3098c41ca36b0
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2024-12-30 08:43:34 +0000

    Practicing the questions

--- BasicDay4/practice-Ques.py
+++ BasicDay4/practice-Ques.py
@@ -16,3 +16,26 @@ print("No. of classrooms - ", classroom)
 # 3. WAP to enter marks of 3 subjects from the user and store them in a dictionary. 
 # start with an empty dictionary and add one by one . use subject name as key and marks as value.
 
+# marks ={}
+# x = int(input("Enter phy  : "))
+# marks.update({"physics" : x})
+
+# x = int(input("Enter maths  : "))
+# marks.update({"Maths" : x})
+
+# x = int(input("Enter chem  : "))
+# marks.update({"Chemistry" : x})
+
+# print(marks)
+
+# 4. Figure out a way to store 9 and 9.0 as separate values in the set.
+
+# This is one of the way to store the same values - by making one as integer and another as string...
+values = {9, "9.0"}
+print(values)
+# Another way is...
+val = {
+    ("float" , 9.0 ),
+    ("int" , 9)
+}
+print(val)
