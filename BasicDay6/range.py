47993e79b3788cb7bf532b93ad6a8a031fecb566
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2025-01-01 18:21:34 +0000

    Learning about range

---
+++ BasicDay6/range.py
@@ -0,0 +1,23 @@
+# range - it is the value upto which it is going
+val = range(10)
+# print(val[0])
+# print(val[2])
+# print(val[4])
+# print(val[6])
+
+# for i in val:
+#     print(i)
+
+# for i in range(10): # starting form 0 # range(stop)
+#     print (i)
+
+# # for i in range(3,10): # range (start, stop)
+# #     print(i)
+
+# for i in range(3,10,2): # range (start, stop, step)
+#     print(i)
+
+# step - kitne se increase krna h 
+# printing all the even number
+for i in range(0,100,2):
+    print(i)
