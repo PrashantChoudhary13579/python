b392cfce859907d0555aeb118506089ba27e6108
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2025-01-01 18:40:32 +0000

    Practice

--- BasicDay6/practice.py
+++ BasicDay6/practice.py
@@ -9,6 +9,24 @@
 #     print(val)
 
 # 3. Printing the multiplication table 
-num = int (input("Enter the number"))
-for i in range(1,11):
-    print(num,"*",i,"=",num*i)
+# num = int (input("Enter the number"))
+# for i in range(1,11):
+#     print(num,"*",i,"=",num*i)
+
+# 4. Write a program to find the sum of first n numbers (using while)
+
+val = 1
+sum = 0
+n = int (input("Enter the value "))
+while val <= n:
+    sum += val
+    val += 1
+print(sum)
+
+# 5. WAP to find the factorial of first n numbers.(using for)
+i = 1
+n = int (input("Enter the value"))
+fact = 1
+for i in range(1,n+1):
+    fact = fact * i 
+print("Factorial of i =",fact)
