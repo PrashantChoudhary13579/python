624ee270fa2cd8a3a1ad34b7f7ee0f9d503239ce
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2025-01-01 18:20:46 +0000

    Practice Questions

---
+++ BasicDay6/practice.py
@@ -0,0 +1,14 @@
+# 1 . printing numbers from 1 to 100
+
+# for val in range(1,101):
+#     print(val)
+
+# 2. Printing numbers from 100 to 1.
+
+# for val in range(100,0,-1):
+#     print(val)
+
+# 3. Printing the multiplication table 
+num = int (input("Enter the number"))
+for i in range(1,11):
+    print(num,"*",i,"=",num*i)
