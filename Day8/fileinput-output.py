029691f6278074fdab1845579f2d46f213854d49
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2025-01-03 18:28:52 +0000

    file

---
+++ Day8/fileinput-output.py
@@ -0,0 +1,11 @@
+# File I/O in python is used to perform various operations on a file such as read, write and appending too.
+# There are various types of files:
+# 1. Text file: .txt , .docx, .log
+#    Those file in which the data is stored in the form of character.
+# 2. Binary file: .mp4 , .mov, .png, .jpeg
+#    videos files, image files - these are binary file and we can manipulate upon them.
+
+# When these files are stored - they all are stored in the form of bits (1,0)
+
+#  RAM - the fastest memory , it is volatile - if system is refreshed it got vanish / all clean 
+#  If we want to store the data for long time we can store in the format of files , and they are stored in ssd /hdd.
