6f3d319134aeb9087a4c61f8b80ed882b4bb256b
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2025-01-05 19:02:44 +0000

    learning about polymorphism

---
+++ Day10/polymorphism.py
@@ -0,0 +1,5 @@
+# Polymorphism - it means ek hi chij ke kai saaare chehre 
+# when the same operator  is allowed to have different meaning according to the context
+
+print (1+2) #3
+print("Apna" + "College")
