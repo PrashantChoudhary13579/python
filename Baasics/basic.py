ddcec8a84e2b164d02f63b180fd9c90ba8de04a6
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2025-01-26 22:48:22 +0530

    Beginner level - basics

---
+++ Baasics/basic.py
@@ -0,0 +1,17 @@
+# printing different data types 
+print("Hello  world")
+print(5)
+print(6.8)
+print(False)
+print("India","USA","Australia")
+print("India","USA","Australia",sep = "/",end = " -  ")
+print("India","USA","Australia")
+
+# integer
+print(1.7e308)
+# boolean
+print(True)
+print(False)
+
+# complex
+print("Complex",4 +5j)
